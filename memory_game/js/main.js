72bd7f69e87ee36996ab60bc505871602c7f95fd
Daniel Kuang <danielkuang@Daniels-MacBook-Pro-3.local>
2019-02-10 19:49:06 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,19 +2,23 @@ var cards =["queen", "queen", "king"];
 
 var cardsInPlay = [];
 
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+}
+
+checkForMatch();
+
 var flipCard = function(cardId){
 
-var cardOne = cards[0];
+cardsInPlay.push(cards[cardId]);
 
-cardsInPlay.push(cardOne);
+console.log("User flipped" + cards[cardId]);
 
-console.log("User flipped" + cardOne);
 
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped" + cardTwo);
 // if cardsInPlay.length === 2
 if (cardsInPlay[0] === cardsInPlay[1]) {
 
@@ -24,3 +28,6 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
           alert("Sorry, try again");
         };
 }
+
+flipCard(0);
+flipCard(2);
