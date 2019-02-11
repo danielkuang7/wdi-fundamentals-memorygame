ca5101c8949710ea33c8d1c37a6871789182d2b2
Daniel Kuang <danielkuang@Daniels-MacBook-Pro-3.local>
2019-02-10 19:27:11 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,26 @@
 var cards =["queen", "queen", "king"];
+
 var cardsInPlay = [];
+
+var flipCard = function(cardId){
+
 var cardOne = cards[0];
+
 cardsInPlay.push(cardOne);
-console.log("User flipped queen");
+
+console.log("User flipped" + cardOne);
+
 var cardTwo = cards[2];
+
 cardsInPlay.push(cardTwo);
-console.log("User flipped king");
+
+console.log("User flipped" + cardTwo);
 // if cardsInPlay.length === 2
 if (cardsInPlay[0] === cardsInPlay[1]) {
+
           alert("You found a match!");
 
       } else {
           alert("Sorry, try again");
         };
+}
